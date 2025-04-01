6a7c935b0b3013ccfbbc52645f3845fe13b8e96d
FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>
2025-04-01 19:36:41 +0530

    Update _jiosaavn.py

--- src/platforms/_jiosaavn.py
+++ src/platforms/_jiosaavn.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2025 AshokShau.
+#  Copyright (c) 2025 AshokShau..
 #  TgMusicBot is an open-source Telegram music bot licensed under AGPL-3.0.
 #  All rights reserved where applicable.
 #
